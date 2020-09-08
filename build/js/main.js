c12daf75f1bf89fbcb5a5194914cf1c576b780ed
Aleksandr <adolinovskiy@gmail.com>
2020-09-08 19:16:28 +0300

    create rules slider

--- build/js/main.js
+++ build/js/main.js
@@ -3061,6 +3061,19 @@ function sliderInit() {
       nav: true,
       navPosition: "bottom"
     });
+    var rulesSlider = tns({
+      container: "#rulesSlider",
+      items: 1.2,
+      slideBy: 1,
+      loop: false,
+      autoplayButton: true,
+      autoplayButtonOutput: false,
+      startIndex: 0,
+      controls: false,
+      navAsThumbnails: true,
+      nav: true,
+      navPosition: "bottom"
+    });
   }
 }
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -14,5 +14,18 @@ function sliderInit() {
 			nav: true,
 			navPosition: "bottom",
 		});
+		var rulesSlider = tns({
+			container: "#rulesSlider",
+			items: 1.2,
+			slideBy: 1,
+			loop: false,
+			autoplayButton: true,
+			autoplayButtonOutput: false,
+			startIndex: 0,
+			controls: false,
+			navAsThumbnails: true,
+			nav: true,
+			navPosition: "bottom",
+		});
 	}
 }
