ecf944c5982c87fd68645cb3d9a5b74ae8d0b486
Aleksandr <adolinovskiy@gmail.com>
2020-09-08 22:08:21 +0300

    create questions block

--- build/js/main.js
+++ build/js/main.js
@@ -3074,6 +3074,19 @@ function sliderInit() {
       nav: true,
       navPosition: "bottom"
     });
+    var rulesSlider = tns({
+      container: "#subjectsSlider",
+      items: 1.2,
+      slideBy: 1,
+      loop: false,
+      autoplayButton: true,
+      autoplayButtonOutput: false,
+      startIndex: 0,
+      controls: false,
+      navAsThumbnails: true,
+      nav: true,
+      navPosition: "bottom"
+    });
   }
 }
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -27,5 +27,18 @@ function sliderInit() {
 			nav: true,
 			navPosition: "bottom",
 		});
+		var rulesSlider = tns({
+			container: "#subjectsSlider",
+			items: 1.2,
+			slideBy: 1,
+			loop: false,
+			autoplayButton: true,
+			autoplayButtonOutput: false,
+			startIndex: 0,
+			controls: false,
+			navAsThumbnails: true,
+			nav: true,
+			navPosition: "bottom",
+		});
 	}
 }
